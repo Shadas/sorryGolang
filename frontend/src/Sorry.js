15a581cae3ac4ef62e9041bb8beb7c1afd70c1e8
Shadas <zhyj2266680@163.com>
2018-05-22 15:04:54 +0800

    重构模板选项，以及默认字幕

--- frontend/src/Sorry.js
+++ frontend/src/Sorry.js
@@ -33,7 +33,12 @@ class Words extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			wordList : [],
+			wordList : [
+				"武林来冲140！",
+				"wow来打大boss！",
+				"帝国来干no mercy！",
+				"金晨曦怒了，撤了撤了",
+			],
 			templateType: "wangjingze",
 			gifId: "",
 		};
@@ -86,6 +91,21 @@ class Words extends Component {
 		case 3:
 			value = this.refs.input3.value;
 			break;
+		case 4:
+			value = this.refs.input4.value;
+			break;
+		case 5:
+			value = this.refs.input5.value;
+			break;
+		case 6:
+			value = this.refs.input6.value;
+			break;
+		case 7:
+			value = this.refs.input7.value;
+			break;
+		case 8:
+			value = this.refs.input8.value;
+			break;
 		default:
 			value = "";
 		}
@@ -97,32 +117,75 @@ class Words extends Component {
 	}
 
 	handleChangeTemplateType() {
+		var newType = this.refs.templateType.value;
+
+		var wordList = [];
+		if (newType === "wangjingze") {
+			wordList = [
+				"武林来冲140！",
+				"wow来打大boss！",
+				"帝国来干no mercy！",
+				"金晨曦怒了，撤了撤了",
+			];
+		} else if (newType === "sorry") {
+			wordList = [
+				"好吧",
+				"就算我武林不练级",
+				"就算我wow不上线",
+				"就算我帝国打一半",
+				"可是我有金晨曦",
+				"有金晨曦你了不起啊",
+				"对不起，有金晨曦就是了不起",
+				"我鸽了他也没办法",
+				"没办法没办法",
+			];
+		}
 		this.setState({
-			templateType: this.refs.templateType.value,
+			templateType: newType,
+			wordList: wordList,
 		});
+
 	}
 
-	renderWord(i) {
-		var thisref = "input"+i;
-		return (
-			<li><input type="text" ref={thisref} value={this.state.wordList[i]} onChange={()=>this.handleChange(i)}/></li>
-		);
+	renderWord() {
+		if (this.state.templateType === "wangjingze") {
+			return (
+				<ul>
+					<li><input type="text" ref="input0" value={this.state.wordList[0]} onChange={()=>this.handleChange(0)}/></li>
+					<li><input type="text" ref="input1" value={this.state.wordList[1]} onChange={()=>this.handleChange(1)}/></li>
+					<li><input type="text" ref="input2" value={this.state.wordList[2]} onChange={()=>this.handleChange(2)}/></li>
+					<li><input type="text" ref="input3" value={this.state.wordList[3]} onChange={()=>this.handleChange(3)}/></li>
+				</ul>
+			);
+		} else if (this.state.templateType === "sorry") {
+			return (
+				<ul>
+					<li><input type="text" ref="input0" value={this.state.wordList[0]} onChange={()=>this.handleChange(0)}/></li>
+					<li><input type="text" ref="input1" value={this.state.wordList[1]} onChange={()=>this.handleChange(1)}/></li>
+					<li><input type="text" ref="input2" value={this.state.wordList[2]} onChange={()=>this.handleChange(2)}/></li>
+					<li><input type="text" ref="input3" value={this.state.wordList[3]} onChange={()=>this.handleChange(3)}/></li>
+					<li><input type="text" ref="input4" value={this.state.wordList[4]} onChange={()=>this.handleChange(4)}/></li>
+					<li><input type="text" ref="input5" value={this.state.wordList[5]} onChange={()=>this.handleChange(5)}/></li>
+					<li><input type="text" ref="input6" value={this.state.wordList[6]} onChange={()=>this.handleChange(6)}/></li>
+					<li><input type="text" ref="input7" value={this.state.wordList[7]} onChange={()=>this.handleChange(7)}/></li>
+					<li><input type="text" ref="input8" value={this.state.wordList[8]} onChange={()=>this.handleChange(8)}/></li>
+				</ul>
+			);
+		}
 	}	
 
 	render() {
 		return (
 			<div>
-				
-				<ul>
-					{this.renderWord(0)}
-					{this.renderWord(1)}
-					{this.renderWord(2)}
-					{this.renderWord(3)}
-				</ul>
-				
+				{this.renderWord()}
 
 				<span>模板</span>
-				<input type="text" ref="templateType" value={this.state.templateType} onChange={()=>this.handleChangeTemplateType()}/>
+
+
+				<select ref="templateType" value={this.state.templateType} onChange={()=>this.handleChangeTemplateType()}>
+					<option value="wangjingze">王境泽</option>
+					<option value="sorry">sorry</option>
+				</select>
 
 				<p>
 				<button onClick={()=>this.Submit()}>
